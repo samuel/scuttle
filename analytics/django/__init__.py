0fc5d1f9066ea7322959bf8a93e3f64b9c26f71f
Samuel Stauffer <samuel@descolada.com>
2009-07-15 18:29:43 -0700

    Added django middlware

---
+++ analytics/django/__init__.py
@@ -0,0 +1,12 @@
+
+# Create a default analytics instance
+
+def create_analytics():
+    from django.conf import settings
+    logger_class_path, logger_args = settings.METRICS_LOGGER
+    class_path = logger_class_path.rsplit('.', 1)
+    logger_class = getattr(__import__(class_path[0], {}, {}, [class_path[1]]), class_path[1])
+    logger = logger_class(**logger_args)
+    return Analytics(logger)
+
+analytics = create_analytics()
