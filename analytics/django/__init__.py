576fafc3a7502caa2cd9e1eeb028dfa10734e942
Samuel Stauffer <samuel@descolada.com>
2009-07-15 18:34:49 -0700

    Fixed name of Django setting.. ANALYTICS_LOGGER instead of METRICS_LOGGER

--- analytics/django/__init__.py
+++ analytics/django/__init__.py
@@ -3,7 +3,7 @@
 
 def create_analytics():
     from django.conf import settings
-    logger_class_path, logger_args = settings.METRICS_LOGGER
+    logger_class_path, logger_args = settings.ANALYTICS_LOGGER
     class_path = logger_class_path.rsplit('.', 1)
     logger_class = getattr(__import__(class_path[0], {}, {}, [class_path[1]]), class_path[1])
     logger = logger_class(**logger_args)
