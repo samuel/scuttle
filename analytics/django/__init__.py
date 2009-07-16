62fd7fafd31504c5f9ed0988477ae5555ea28a60
Samuel Stauffer <samuel@descolada.com>
2009-07-15 18:38:53 -0700

    Added a missing import to django/__init__

--- analytics/django/__init__.py
+++ analytics/django/__init__.py
@@ -3,6 +3,7 @@
 
 def create_analytics():
     from django.conf import settings
+    from analytics import Analytics
     logger_class_path, logger_args = settings.ANALYTICS_LOGGER
     class_path = logger_class_path.rsplit('.', 1)
     logger_class = getattr(__import__(class_path[0], {}, {}, [class_path[1]]), class_path[1])
