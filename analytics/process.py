093f190a01d28e35ca2a87afca3a848d6f6c2255
Samuel Stauffer <samuel@descolada.com>
2010-11-03 17:55:10 -0700

    ProcessLogs now gzips and uploads to S3

--- analytics/process.py
+++ analytics/process.py
@@ -10,19 +10,70 @@ from __future__ import with_statement
 
 import base64
 import datetime
+import gzip
 import os
 import sys
 import time
 import uuid
+from contextlib import closing
 
 def unique():
     value = uuid.uuid4().bytes
     return base64.b64encode(value, ('-', '_')).rstrip('=')
 
+
+from httplib import BadStatusLine
+from boto.exception import S3ResponseError
+from boto.s3.bucket import Bucket
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+class S3Uploader(object):
+    def __init__(self, aws_key, aws_secret_key):
+        self.aws_key = aws_key
+        self.aws_secret_key = aws_secret_key
+        self.conn = S3Connection(self.aws_key, self.aws_secret_key)
+
+    def retry(self, func, *args, **kwargs):
+        """retry all commands a few times func because S3 occasionally returns 500 errors"""
+
+        retry_sleep = 0.2
+        for i in range(3):
+            try:
+                return func(*args, **kwargs)
+            except (S3ResponseError, BadStatusLine, ValueError), exc:
+                # ValueError is raised when httplib doesn't handle an invalid response properly
+                # "ValueError: invalid literal for int() with base 16: ''"
+
+                if not isinstance(exc, (BadStatusLine, ValueError)) and exc.status != 500:
+                    raise
+
+                last_error = exc
+                time.sleep(retry_sleep)
+                retry_sleep *= 2
+
+        raise last_error
+
+    def upload(self, filename, bucket, key_name, metadata=None):
+        bucket = Bucket(self.conn, bucket)
+        key = Key(bucket, key_name)
+        key.content_type = "application/x-gzip"
+        if metadata:
+            for k, v in metadata.items():
+                key.set_metadata(k, str(v))
+        with open(filename, "rb") as fp:
+            self.retry(key.set_contents_from_file, fp)
+            # self.retry(key.set_acl, '')
+        # Verify it exists (might need to wait a bit before it shows up)
+        # success = bool(self.retry(bucket.lookup, key_name))
+
 class ProcessLogs(object):
-    def __init__(self, input_path):
+    def __init__(self, input_path, aws_key, aws_secret_key, aws_bucket):
+        self.aws_key = aws_key
+        self.aws_secret_key = aws_secret_key
+        self.aws_bucket = aws_bucket
         self.input_path = input_path
-        self.input_files = os.listdir(input_path)
+        self.input_files = [f for f in os.listdir(input_path) if f.endswith('.log')]
+        self.input_files.sort()
         self.output_files = {}
         self.max_file_size = 128*1024*1024
         self.output_path = os.path.join(self.input_path, "processed")
@@ -35,41 +86,41 @@ class ProcessLogs(object):
                 except OSError, exc:
                     if exc.errno != 17: # If the error isn't "file exists" then reraise
                         raise
-
+    
     def close_all_output_files(self):
         for base_filename, fp in self.output_files.itervalues():
             fp.close()
         self.output_files = {}
-
+    
     def get_optout_file(self, event, timestamp):
         date = datetime.datetime.utcfromtimestamp(timestamp)
         base_filename = "ev=%s/dt=%s" % (event, date.strftime("%Y-%m-%d"))
-
+        
         if base_filename in self.output_files:
             return self.output_files[base_filename]
-
+        
         base_path = os.path.join(self.output_path, base_filename)
-
+        
         if not os.path.exists(base_path):
             try:
                 os.makedirs(base_path)
             except OSError, exc:
                 if exc.errno != 17: # If the error isn't "file exists" then reraise
                     raise
-
+        
         filename = os.path.join(base_path, "%d-%s.log" % (timestamp, unique()))
         fp = open(filename, "a")
         self.output_files[base_filename] = (base_filename, fp)
-
+        
         return base_filename, fp
-
+    
     def write_event(self, event, timestamp, attributes):
         base_filename, fp = self.get_optout_file(event, timestamp)
         fp.write("%s\x01%s\n" % ("%.3f" % timestamp if isinstance(timestamp, float) else timestamp, attributes))
         if fp.tell() > self.max_file_size - 1024:
             fp.close()
             del self.output_files[base_filename]
-
+    
     def process_file(self, filename, path):
         working_filename = os.path.join(self.working_path, filename)
         os.rename(path, working_filename)
@@ -89,14 +140,50 @@ class ProcessLogs(object):
                 self.write_event(event, timestamp, attributes)
         os.rename(working_filename, os.path.join(self.trash_path, filename))
         self.close_all_output_files()
-
+    
     def run(self):
         for filename in self.input_files:
             path = os.path.join(self.input_path, filename)
             if os.path.isfile(path):
                 self.process_file(filename, path)
+        
+        s3_uploader = S3Uploader(self.aws_key, self.aws_secret_key)
+        
+        for root, dirs, files in os.walk(self.output_path):
+            to_remove = []
+            for d in dirs:
+                full = os.path.join(root, d)
+                if d[0] == '.':
+                    to_remove.append(d)
+            for d in to_remove:
+                dirs.remove(d)
+            
+            files.sort()
+            for filename in files:
+                if filename[0] == '.':
+                    continue
+                path = os.path.join(root, filename)
+                
+                if not filename.endswith(".gz"):
+                    gzip_name = path+".gz"
+                    with open(path, "rb") as infp:
+                        with closing(gzip.open(gzip_name, "wb")) as outfp:
+                            while True:
+                                block = infp.read(1024*128)
+                                if not block:
+                                    break
+                                outfp.write(block)
+                    os.unlink(path)
+                else:
+                    gzip_name = path
+
+                key = gzip_name[len(self.output_path)+1:]
+                s3_uploader.upload(gzip_name, self.aws_bucket, key)
+                os.unlink(gzip_name)
 
 if __name__ == "__main__":
     input_path = sys.argv[1]
-    output_path = sys.argv[2]
-    ProcessLogs(input_path, output_path).run()
+    aws_key = sys.argv[2]
+    aws_secret_key = sys.argv[3]
+    aws_bucket = sys.argv[4]
+    ProcessLogs(input_path, aws_key, aws_secret_key, aws_bucket).run()
